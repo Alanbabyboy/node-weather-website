91e41c7585ef369d7746ffe24d60b7045ce3e770
Alanbabyboy <alanlloydz11@gmail.com>
2022-06-06 13:25:46 +0530

    added changed forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,8 @@ request({url,json:true},(error,{body})=>{
   }else if(body.error){
    callback('Unable to find location.',undefined)
   }else{
-   callback(undefined, body.current.weather_descriptions[0] + '.It is currently ' +body.current.temperature + ' degrees. There is a ' + body.current.precip + '% chance of rain.')
+    console.log(body)
+   callback(undefined, body.current.weather_descriptions[0] + '.It is currently ' +body.current.temperature + ' degrees. There is a ' + body.current.precip + '% chance of rain. ' +' The humidity is ' + body.current.humidity + '%.')
   }
 })
 }
